6cb76ae4a154cb3650bebc02f8b5e5e981d315c6
horiga <h.horiga@gmail.com>
2020-10-12 01:54:49 +0900

    test code

--- src/main/kotlin/org/horiga/study/armeria/http/handler/BookHandler.kt
+++ src/main/kotlin/org/horiga/study/armeria/http/handler/BookHandler.kt
@@ -5,23 +5,23 @@ import com.linecorp.armeria.server.annotation.Get
 import com.linecorp.armeria.server.annotation.Param
 import com.linecorp.armeria.server.annotation.Produces
 import org.horiga.study.armeria.http.service.Book
-import org.horiga.study.armeria.http.service.IsbnService
+import org.horiga.study.armeria.http.service.BookService
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
 
 @Component
-class IsbnHandler(val isbnService: IsbnService) {
+class BookHandler(val bookService: BookService) {
 
     companion object {
-        val log = LoggerFactory.getLogger(IsbnHandler::class.java)!!
+        val log = LoggerFactory.getLogger(BookHandler::class.java)!!
     }
 
     @Throws(Exception::class)
     @Get("/book")
     @Produces("application/json; charset=utf-8") // for JacksonResponseConverterFunction
     fun findByIsbn(
-        @Param("isbn") isbn: String = "978-4-7808-0204-7",
+        @Param("isbn") @Default("true") isbn: String = "978-4-7808-0204-7",
         @Param("cache") @Default("true") cache: Boolean
-    ): Mono<Book> = isbnService.findByIsbn(isbn, cache)
+    ): Mono<Book> = bookService.findByIsbn(isbn, cache)
 }
